65506feb22a18948c4859a371a24f2d2f49865ba
Ebod Shojaei <ebod.shojaei@gmail.com>
2025-01-15 05:52:33 -0800

    (update) getEmbedding returns average similarity score for batch inputs 👥

--- app/agent/src/plugins/nlu/nlp.ts
+++ app/agent/src/plugins/nlu/nlp.ts
@@ -30,13 +30,52 @@ export class NLPModel implements TextEmbeddingType {
 
   /**
    * Function to get the embedding of the input text
-   * @param text is the input text to get the embedding of
+   * @param example is the input text to get the embedding of
    * @returns Promise<number[]> is the embedding of the input text
    */
-  async getEmbedding(text: string): Promise<number[]> {
-    const tensor = await this.model.embed([text]);
+  async getEmbedding(example: string | string[]): Promise<number[]> {
+    // If length of text is greater than 1, compute the averaged embedding
+    if (Array.isArray(example)) return this.getBatchEmbedding(example);
+
+    const tensor = await this.model.embed([example]);
     const embedding = await tensor.array();
     tensor.dispose();
     return embedding[0];
+  }
+
+  /**
+   * Function to compute the averaged embedding of the input examples
+   * @param examples is the input examples to compute the averaged embedding of
+   * @returns Promise<number[]> is the averaged embedding of the input examples
+   */
+  private async getBatchEmbedding(examples: string[]): Promise<number[]> {
+    if (!examples || examples.length === 0) {
+      throw new Error('Input examples array cannot be empty.');
+    }
+
+    // Batch embedding for efficiency
+    const tensors = await this.model.embed(examples);
+    const embeddings = await tensors.array(); // Extract the embeddings as arrays
+    tensors.dispose(); // Free memory used by tensors
+
+    // Calculate the element-wise sum of embeddings
+    const summedEmbedding = embeddings.reduce(
+      (sum: number[], embedding: number[]) =>
+        sum.map((val, i) => val + embedding[i]),
+      Array(embeddings[0].length).fill(0), // Initialize a zeroed vector of the same size
+    );
+
+    // Calculate the average embedding
+    const avgEmbedding = summedEmbedding.map(
+      (val: number) => val / embeddings.length,
+    );
+
+    // Normalize the averaged embedding
+    const magnitude = Math.sqrt(
+      avgEmbedding.reduce((sum: number, val: number) => sum + val * val, 0),
+    );
+    return magnitude > 0
+      ? avgEmbedding.map((val: number) => val / magnitude)
+      : avgEmbedding;
   }
 }
